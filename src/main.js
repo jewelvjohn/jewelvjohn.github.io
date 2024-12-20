fa1d1e409990231f3f3bf7780f570b8004384cbc
Jewel John <jeweljohnl0v3@gmail.com>
2024-12-20 11:58:39 +0530

    additional sections

--- src/main.js
+++ src/main.js
@@ -1,11 +1,85 @@
 import "@lottiefiles/lottie-player";
 import {create} from '@lottiefiles/lottie-interactivity';
 
-const instagram = document.getElementById("instagram");
-instagram.onload = (e) => {
-    create({
-        player:'#instagram',
-        mode:"cursor",
-        actions: [{type: "hold"}]
+let resumeButton, expertiseButton, backButtons;
+let mainSection, resumeSection, expertiseSection;
+
+var currentSection = 0;
+
+function transition(from, to) {
+    const fadeOut = from.animate([
+        {
+            opacity: 1
+        }, {
+            opacity: 0
+        }
+    ], {
+        duration: 250,
+        fill: "forwards",
+        easing: "ease-out"
+    });
+
+    fadeOut.addEventListener("finish", () => {
+        from.style.display = 'none';
+        to.style.display = 'flex';
+        const fadeIn = to.animate([
+            {
+                opacity: 0
+            }, {
+                opacity: 1
+            }
+        ], {
+            duration: 250,
+            fill: "forwards",
+            easing: "ease-in"
+        });
+    }, {once: true});
+}
+
+function gotoMain() {
+    if(currentSection !== 0) {
+        if(currentSection === 1) transition(resumeSection, mainSection);
+        else if(currentSection === 2) transition(expertiseSection, mainSection);
+        currentSection = 0;
+    }
+}
+
+function gotoResume() {
+    if(currentSection !== 1) {
+        transition(mainSection, resumeSection);
+        currentSection = 1;
+    }
+}
+
+function gotoExpertise() {
+    if(currentSection !== 2) {
+        transition(mainSection, expertiseSection);
+        currentSection = 2;
+    }
+}
+
+function init() {
+    const instagram = document.getElementById("instagram");
+    instagram.onload = (e) => {
+        create({
+            player:'#instagram',
+            mode:"cursor",
+            actions: [{type: "hold"}]
+        });
+    }
+
+    backButtons = document.querySelectorAll(".back-button");
+    resumeButton = document.querySelector("#resume-button");
+    expertiseButton = document.querySelector("#expertise-button");
+    mainSection = document.querySelector("#main-section");
+    resumeSection = document.querySelector("#resume-section");
+    expertiseSection = document.querySelector("#expertise-section");
+
+    resumeButton.onclick = gotoResume;
+    expertiseButton.onclick = gotoExpertise;
+    backButtons.forEach((button) => {
+        button.onclick = gotoMain;
     });
 }
+
+init();
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite'
 import path from 'path'
+import copy from 'rollup-plugin-copy';
 
 const root = path.resolve(__dirname, 'src');
 const outDir = path.resolve(__dirname, 'dist');
@@ -11,6 +12,12 @@ export default defineConfig({
         outDir: outDir,
         emptyOutDir: true,
         rollupOptions: {
+            plugins: [
+            copy({
+                targets: [{src: 'src/resources/**/*', dest: 'dist/'}],
+                hook: 'writeBundle',
+                flatten: false
+            })],
             input: {
                 main: path.resolve(root, 'index.html')
             }
